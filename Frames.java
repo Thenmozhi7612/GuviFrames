c302c9957a3b57aad065c8fd7d10103a74012098
Thenmozhi7612 <thenmozhaisarathy@gmail.com>
2025-06-17 22:06:59 +0530

    Frames.java

--- Frames.java
+++ Frames.java
@@ -7,60 +7,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-
 
 public class Frames {
-	
-	
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://the-internet.herokuapp.com/nested_frames");
 		driver.manage().window().maximize();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		
-		driver.switchTo().frame("frame-top");
-		List<WebElement> frames = driver.findElements(By.xpath("//frame"));
-		Assert.assertEquals(frames.size(),3,"There should be three frames on the page.");
-		
-		driver.switchTo().frame("frame-left");
-		
-		WebElement leftFrameText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
-		Assert.assertEquals(leftFrameText.getText(),"LEFT", "The left frame should have the visible text 'LEFT'.");
-		System.out.println(driver.getTitle());
+		List<WebElement> parentFrames = driver.findElements(By.xpath("//frame"));
 
-		driver.switchTo().parentFrame();
-		driver.switchTo().frame("frame-middle");
-		
-		WebElement middleFrameText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
-		Assert.assertEquals(middleFrameText.getText(),"MIDDLE", "The middle frame should have the visible text 'MIDDLE'.");
-		
-		driver.switchTo().parentFrame();
-		driver.switchTo().frame("frame-right");
-		
-		WebElement rightFrameText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
-		Assert.assertEquals(rightFrameText.getText(),"RIGHT", "The right frame should have the visible text 'RIGHT'.");
-		
-		
-		driver.switchTo().parentFrame();
-		driver.switchTo().parentFrame();
-		
-driver.switchTo().frame("frame-bottom");
-		
-		WebElement bottomFrameText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
-		Assert.assertEquals(bottomFrameText.getText(),"BOTTOM", "The bottom frame should have the visible text 'BOTTOM'.");
-		
-		driver.switchTo().parentFrame();
-		driver.switchTo().parentFrame();
-		
-		driver.switchTo().frame("frame-top");
-		
-		driver.quit();
+		for (WebElement parentFrame : parentFrames) {
+			driver.switchTo().frame(parentFrame);
+			List<WebElement> frames = driver.findElements(By.xpath("//frame"));
+			int i = frames.size();
+			if (i == 3) {
+				System.out.println("Three frames on the page");
+				for (WebElement frame : frames) {
+					driver.switchTo().frame(frame);
+					String pageSource = driver.getPageSource();
+					if (pageSource.contains("LEFT")) {
+						System.out.println("IN LEFTFRAME");
+					}
+					if (pageSource.contains("MIDDLE")) {
+						System.out.println("IN MIDDLEFRAME");
+					}
+					if (pageSource.contains("RIGHT")) {
+						System.out.println("IN RIGHTFRAME");
+					}
+					driver.switchTo().parentFrame();
+				}
+			} else {
+				String pageSource = driver.getPageSource();
+				if (pageSource.contains("BOTTOM")) {
+					System.out.println("IN BOTTOMFRAME");
+				}
+			}
+
+			driver.switchTo().parentFrame();
+		}
+
 	}
-
 }
